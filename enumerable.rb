b45035b01af358a7836a2408df53f3b54f8dd65b
Kelvin Liang <kelvin9877@gmail.com>
2019-05-24 23:55:08 +0800

    solving map & inject method

--- enumerable.rb
+++ enumerable.rb
@@ -12,23 +12,103 @@ module Enumerable
       yield(self[i], i)
     end
     self  
-
   end
 
   def my_select
+    res = []
+    for i in 0..self.size-1
+      if yield(self[i])
+          res << self[i]
+      end  
+    end
+    return res
   end
 
-  def my_all
+  def my_all?
+    res = true
+    for i in 0..self.size-1
+      if !yield(self[i])
+          res = false
+      end  
+    end
+    return res
+  end
+
+  def my_any?
+    res = false
+    for i in 0..self.size-1
+      if yield(self[i])
+          res = true
+      end  
+    end
+    return res
+  end
+
+  def my_none?
+    res = true
+    for i in 0..self.size-1
+      if yield(self[i])
+          res = false
+      end  
+    end
+    return res
+  end
+
+  def my_count
+    res = 0
+    for i in 0..self.size-1
+      if yield(self[i])
+          res += 1
+      end  
+    end
+    return res
+
+  end
+
+  def my_map
+    res = []
+    for i in 0..self.size-1
+      res << yield(self[i])
+    end
+    return res
+  end
+
+  def my_inject
+
   end
 
 end
 
 
-# [3, 4, 5].my_each {|x| puts x }
+[3, 4, 5].my_each {|x| p x }
 
 hash = Hash.new
-%w(cat dog wombat).my_each_with_index { |item, index|
+['cat', 'dog', 'wombat'].my_each_with_index { |item, index|
   hash[item] = index
 }
-
 p hash
+
+p [1,2,3,4,5].my_select{ |num|  num.even? }   #=> [2, 4]
+
+p %w[ant bear cat].my_all?{ |word| word.length >= 3 } #=> true
+p %w[ant bear cat].my_all?{ |word| word.length >= 4 } #=> false
+
+p %w[ant bear cat].my_any?{ |word| word.length >= 3 } #=> true
+p %w[ant bear cat].my_any?{ |word| word.length >= 4 } #=> true
+
+%w{ant bear cat}.my_none?{ |word| word.length == 5 } #=> true
+%w{ant bear cat}.my_none?{ |word| word.length >= 4 } #=> false
+
+ary = [1, 2, 4, 2]
+p ary.my_count{ |x| x%2==0 }                         #=> 3
+
+p [1,2,3,4].my_map{ |i| i*i }                           #=> [1, 4, 9, 16]
+
+
+# (5..10).inject { |sum, n| sum + n }            #=> 45
+# (5..10).inject(1) { |product, n| product * n } #=> 151200
+# # find the longest word
+# longest = %w{ cat sheep bear }.inject do |memo, word|
+#    memo.length > word.length ? memo : word
+# end
+# longest                                        #=> "sheep"
