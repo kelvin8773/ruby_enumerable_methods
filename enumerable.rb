a85776de62469ede1bdc368c11e2fa740a8d7db4
Kelvin Liang <kelvin9877@gmail.com>
2019-05-24 22:30:01 +0800

    finish my_each method

--- enumerable.rb
+++ enumerable.rb
@@ -1,10 +1,17 @@
 
 module Enumerable
   def my_each
-    # your code here
+    for i in 0..self.size-1
+      yield(self[i])
+    end
+    self  
   end
 
   def my_each_with_index
+    for i in 0..self.size-1
+      yield(self[i], i)
+    end
+    self  
 
   end
 
@@ -15,3 +22,13 @@ module Enumerable
   end
 
 end
+
+
+# [3, 4, 5].my_each {|x| puts x }
+
+hash = Hash.new
+%w(cat dog wombat).my_each_with_index { |item, index|
+  hash[item] = index
+}
+
+p hash
