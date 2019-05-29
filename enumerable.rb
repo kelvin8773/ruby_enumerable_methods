d794f89a3fd224b6052d1e03515f3909bfafe8ac
Kelvin Liang <kelvin9877@gmail.com>
2019-05-29 13:08:59 +0800

    remove test cases

--- enumerable.rb
+++ enumerable.rb
@@ -66,46 +66,11 @@ module Enumerable
 end
 
 
-#  Test case below ...  
-
-[3, 4, 5].my_each {|x| p x }
-
-hash = Hash.new
-['cat', 'dog', 'wombat'].my_each_with_index { |item, index|
-  hash[item] = index
-}
-p hash
-
-p [1,2,3,4,5].my_select{ |num|  num.even? }   #=> [2, 4]
-
-p %w[ant bear cat].my_all?{ |word| word.length >= 3 } #=> true
-p %w[ant bear cat].my_all?{ |word| word.length >= 4 } #=> false
-
-p %w[ant bear cat].my_any?{ |word| word.length >= 3 } #=> true
-p %w[ant bear cat].my_any?{ |word| word.length >= 4 } #=> true
-
-%w{ant bear cat}.my_none?{ |word| word.length == 5 } #=> true
-%w{ant bear cat}.my_none?{ |word| word.length >= 4 } #=> false
-
-ary = [1, 2, 4, 26]
-p ary.my_count{ |x| x%2==0 }                         #=> 3
-
-p ary.my_count
-
-
-p [1,2,3,4].my_map{ |i| i*i }                        #=> [1, 4, 9, 16]
-
-mlt = Proc.new{|x| x*x } 
-
-p [1,2,3,4].my_map(&mlt)
-
-
-p [5,6,7,8,9,10].my_inject{ |sum, n| sum + n }               #=> 45
-p [5,6,7,8,9,10].my_inject(1){ |product, n| product * n }    #=> 151200
 
 def muptiply_els(arr)
   arr.my_inject(1){|product, n| product * n}
 end
+
 p muptiply_els([2, 4, 5])            # => 40
 
 
